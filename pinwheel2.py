766fc8221bb0f04331324ba358ba2caa70d0eb79
lunalucas123 <geovannym123@gmail.com>
2021-03-10 13:31:12 -0600

    json

--- pinwheel2.py
+++ pinwheel2.py
@@ -10,14 +10,11 @@ user_input = input("Enter form : ").lower()
 user_input = user_input.replace(' ','+')
 fixed_form = user_input[0].upper() + user_input[1:]
 
-# print(fixed_form)
+with open('IRS.csv', mode='w') as csv_file:
+   fieldnames = ['form_number', 'title', 'year']
+   writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+   writer.writeheader()
 
-# with open('IRS.csv', mode='w') as csv_file:
-#    fieldnames = ['form_number', 'title', 'year']
-#    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-#    writer.writeheader()
-
-obj_form_number = {}
 list_form_number = []
 list_title = []
 list_year = []
@@ -34,11 +31,9 @@ def open_pages(index):
    
     results_table = soup.find('table', attrs={"class": "searchFieldsTable"})
     results_page = results_table.find_all('tr')
-    # print(results_page)
     # results_number = results_page.find('tr', attrs={"class": "ShowByColumn"} )
     for r in range(len(results_page)):
         result_number = results_page[r].find_all('th', 'ShowByColumn')
-        # print(result_number)
 
         for e in result_number: 
             page_max_num = e.text.strip()
@@ -49,58 +44,29 @@ def open_pages(index):
 
     table = soup.find('table', attrs={"class": "picklist-dataTable"})
     first_page_rows = table.find_all('tr')
-    # print(type(first_page_rows))
     first_page_rows_len = len(first_page_rows)
     # print(first_page_rows)
     
 
     for x in range(first_page_rows_len):
         form_number = first_page_rows[x].find_all('td', 'LeftCellSpacer')
-        # print(form_number)
         title = first_page_rows[x].find_all('td', 'MiddleCellSpacer')
-        # print(title)
         year = first_page_rows[x].find_all('td', 'EndCellSpacer')
 
-        new_form = fixed_form.replace('+',' ')
-        form_result_list = []
-        title_result_list = []
-        year_result_list = []
-        for f in form_number: 
-            list_form_number.append(f.text.strip())
-            for f in list_form_number:
-                obj_form_number = {"form_number" : f }
-            
-            y = json.dumps(obj_form_number)
-            # print(y)
 
-
-        for t in title: 
-            list_title.append(t.text.strip())
-            for t in list_title:
-                obj_form_number['title'] = t
-            
-            y = json.dumps(obj_form_number)
-
-            # print(y)
-
-        for y in year: 
-            list_year.append(y.text.strip())
-            for y in list_year:
-                obj_form_number['year'] = y
-            
-            y = json.dumps(obj_form_number)
-
-            # print(y)
-    
-    with open('data.txt', 'w') as outfile:
-        json.dump(y, outfile)        
+        for e in form_number: 
+            list_form_number.append(e.text.strip())
+        for e in title: 
+            list_title.append(e.text.strip())
+        for e in year: 
+            list_year.append(e.text.strip())
         
     
     # print(list_form_number,list_title, list_year)
     
-    # data = { 'Form_number': form_result_list,'Title':title_result_list, 'year':year_result_list}
-    # df = DataFrame(data, columns = ['Form_number','Title','year'])
-    # df.to_csv(r'/Users/geovannymolina/Desktop/IRS.csv')
+    data = { 'Form_number': list_form_number,'Title':list_title, 'year':list_year}
+    df = DataFrame(data, columns = ['Form_number','Title','year'])
+    df.to_csv(r'/Users/geovannymolina/Desktop/IRS.csv')
     index = index + 25
 
     while index < page_max_sliced:
